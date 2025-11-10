d07e44e9d34a1f991bbf49efbee2f0e19cc9c20c
James Carl Sitsit <jamescarlsitsit@gmail.com>
2025-11-10 18:13:20 +0800

    updates for the gsap workshop

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,13 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
-  // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+    gsap.from('#green-box', {
+      x: 250,
+      
+    });
+  }, []);
 
   return (
     <main>
